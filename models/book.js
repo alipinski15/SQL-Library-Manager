bb14d6f2c0666707202f4dbd95e04e91cc8c9115
alipinski15 <58602252+alipinski15@users.noreply.github.com>
2020-06-12 15:42:26 -0700

    Update

---
+++ models/book.js
@@ -0,0 +1,20 @@
+'use strict';
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Book extends Sequelize.Model {}
+  Book.init({
+    title: {
+      type: Sequelize.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'The "Title" is required'
+        }
+      }      
+    },
+    author: Sequelize.STRING,
+    genre: Sequelize.STRING,
+    year: Sequelize.INTEGER
+  }, {sequelize});
+  return Book;
+};
